7f497dc013155b6ea565187aaab1ab01018eb6e6
root <root@Teerth.localdomain>
2023-02-19 15:34:52 +0530

    initial commit

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,111 @@
+import { contractAddresses, abi } from "../constants"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { useEffect, useState } from "react"
+import { useNotification } from "web3uikit"
+import { ethers } from "ethers"
+// import { useNotification } from "web3uikit"
+
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled, Moralis } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    // console.log(chainId)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayer, setNumPlayer] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+
+    const dispatch = useNotification()
+
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "enterRaffle",
+        msgValue: entranceFee,
+        params: {},
+    })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getNumberOfPlayers",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    async function updateUI() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayerFromCall = (await getNumberOfPlayers()).toString()
+        const recentWinnerFromCall = (await getRecentWinner()).toString()
+        setEntranceFee(entranceFeeFromCall)
+        setNumPlayer(numPlayerFromCall)
+        setRecentWinner(recentWinnerFromCall)
+    }
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const handleSuccess = async (tx) => {
+        await tx.wait(1)
+        handleNotification(tx)
+        updateUI()
+    }
+    const handleNotification = () => {
+        dispatch({
+            type: "info",
+            message: "Transaction Conplete!",
+            title: "Transaction Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
+    return (
+        <div>
+            Hii from Lottery entrance!
+            {raffleAddress ? (
+                <div>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                        onClick={async function () {
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
+                        }}
+                        disabled={isLoading || isFetching}
+                    >
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounde-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
+                    </button>
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} Eth</div>
+                    <div>Number Of Players: {numPlayer}</div>
+                    Recent Winner: {recentWinner}
+                </div>
+            ) : (
+                <div> Please connect to a supported chain</div>
+            )}
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <MoralisProvider>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
+    )
 }
